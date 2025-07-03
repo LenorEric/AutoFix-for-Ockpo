12b64af137c941bb95b955d3ffe2dc2d6a162f2f
Lenor Tang <40317566+LenorEric@users.noreply.github.com>
2025-07-03 13:48:15 +0800

    skip list function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
 use std::path::Path;
 use std::{fs, env, io};
-use std::io::prelude::*;
+use std::io::{prelude::*, BufReader};
 use std::process::Command;
 
 const FAKE_ENCRYPTED_HEADER: [u8; 16] = [0x62, 0x14, 0x23, 0x65, 0x3f, 0x00, 0x13, 0x01,
     0x0d, 0x0a, 0x0d, 0x0a, 0x0d, 0x0a, 0x0d, 0x0a];
+
+fn read_skip_list() -> Vec<String> {
+    let mut skip_list = Vec::new();
+    if let Ok(file) = fs::File::open("list.txt") {
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            if let Ok(ext) = line {
+                skip_list.push(ext.trim().to_string());
+            }
+        }
+    }
+    skip_list
+}
+
+fn should_skip_file(path: &Path, skip_list: &[String]) -> bool {
+    if let Some(extension) = path.extension() {
+        if let Some(ext_str) = extension.to_str() {
+            for skip_ext in skip_list {
+                let skip_ext = skip_ext.trim_start_matches('.');
+                if ext_str.eq_ignore_ascii_case(skip_ext) {
+                    return true;
+                }
+            }
+        }
+    }
+    false
+}
 
 fn are_same_file(path1: &str, path2: &str) -> io::Result<bool> {
     let canonical_path1 = fs::canonicalize(path1)?;
@@ -32,7 +59,6 @@ fn decrypt_file(src: &str) -> io::Result<()> {
     Ok(())
 }
 
-
 fn check_req() -> bool {
     let file_name = "code.exe";
     if Path::new(file_name).exists() {
@@ -42,7 +68,7 @@ fn check_req() -> bool {
     }
 }
 
-fn recursive_decrypt(father_path: &Box<Path>, proc_path: &Box<Path>) -> io::Result<()> {
+fn recursive_decrypt(father_path: &Box<Path>, proc_path: &Box<Path>, skip_list: &[String]) -> io::Result<()> {
     let current_exe_path = env::current_exe()?;
     for entry in fs::read_dir(proc_path)? {
         let entry = entry?;
@@ -51,12 +77,18 @@ fn recursive_decrypt(father_path: &Box<Path>, proc_path: &Box<Path>) -> io::Resu
             if are_same_file(current_exe_path.to_str().unwrap(), path.to_str().unwrap())? {
                 continue;
             }
+
+            if should_skip_file(&path, skip_list) {
+                println!("Skipping: {:?}", path);
+                continue;
+            }
+
             println!("Checking: {:?}", path);
             if check_is_encrypted(path.to_str().unwrap()) {
                 decrypt_file(path.to_str().unwrap())?;
             }
         } else if path.is_dir() {
-            recursive_decrypt(father_path, &Box::from(path.clone()))?
+            recursive_decrypt(father_path, &Box::from(path.clone()), skip_list)?
         }
     }
     Ok(())
@@ -67,7 +99,10 @@ fn main() -> io::Result<()> {
         println!("Requirement not found");
         return Ok(());
     }
+
+    let skip_list = read_skip_list();
+
     let current_dir = env::current_dir()?;
-    recursive_decrypt(&Box::from(current_dir.clone()), &Box::from(current_dir.clone()))?;
+    recursive_decrypt(&Box::from(current_dir.clone()), &Box::from(current_dir.clone()), &skip_list)?;
     Ok(())
 }
